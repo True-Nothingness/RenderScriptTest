b2501adb9b927d63e46d0abf5ee104a63da405d9
Light <ogatatsumi1152@gmail.com>
2024-12-15 15:21:33 +0700

    Hotfixes

--- app/src/main/res/raw/grayscale.glsl
+++ app/src/main/res/raw/grayscale.glsl
@@ -1,13 +1,13 @@
 #version 320 es
 precision mediump float;
 
-in vec2 vTexCoord;       // Interpolated texture coordinates
+in vec2 v_TexCoord;       // Interpolated texture coordinates
 out vec4 FragColor;
 
 uniform sampler2D uTexture; // Input texture
 
 void main() {
-    vec4 color = texture(uTexture, vTexCoord);
+    vec4 color = texture(uTexture, v_TexCoord);
     float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114)); // Grayscale weights
     FragColor = vec4(vec3(gray), color.a); // Set RGB to gray, preserve alpha
 }
